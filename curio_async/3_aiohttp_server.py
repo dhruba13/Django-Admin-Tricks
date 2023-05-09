1b6af707c1b524be7dfb5832b03f303b5e621097
Maxim <info@twinboards.at>
2023-05-09 13:10:27 +0200

    update stats improve aiohttp_server

--- curio_async/3_aiohttp_server.py
+++ curio_async/3_aiohttp_server.py
@@ -1,11 +1,18 @@
-from aiohttp.web import Response, Application, run_app, StreamResponse
+from aiohttp.web import Application, run_app, StreamResponse
 
-HEADERS = b'HTTP/1.1 200 OK\r\nContent-type: text/html\r\nConnection: close\r\n\r\n'
+# HEADERS = b'HTTP/1.1 200 OK\r\nContent-type: text/html; charset=utf-8\r\nConnection: close\r\n\r\n'
 BODY = '<!DOCTYPE html><html><head><meta charset="utf-8"><title></title></head><body>Hi</body></html>'
 
 
 async def echo_client(request):
-    return Response(body=BODY, content_type = 'text/html', charset = 'utf-8')
+    stream = StreamResponse()
+    stream.content_type = 'text/html'
+    stream.charset= 'utf-8'
+
+    await stream.prepare(request)
+    #await stream.write(HEADERS)
+    await stream.write(BODY.encode('utf-8'))
+    await stream.write_eof()
 
 if __name__ == '__main__':
     app = Application()
--- curio_async/__init__.py
+++ curio_async/__init__.py
@@ -1,6 +1,24 @@
-FILE_BASE, *__ = __file__.rpartition('\\')
+from pathlib import Path
 
-FILE_LIST = ('docs\\pg.pdf', '1_curi.py', '1_tri.py', '1_async.py')
+COUNT = 1
+FILE_BASE = Path(__file__).parent
+FILE_LIST = (FILE_BASE / 'docs' / 'pg.pdf', FILE_BASE / '1_curi.py', FILE_BASE / '1_tri.py', FILE_BASE / '1_async.py') * 1
+
 
 def REPORT(file, elapsed):
     print(f'{file} executed in {elapsed:0.5f} seconds.')
+
+
+# import shutil
+
+# FILE_LIST = [FILE_BASE / 'examples' / f'pg{i}.pdf' for i in range(COUNT)]
+
+# def generate_files():
+#     base = FILE_BASE / 'docs' / 'pg.pdf'
+
+#     Path.mkdir(FILE_BASE / 'examples', exist_ok=True)
+#     for name in FILE_LIST:
+#         shutil.copy(base, name)
+
+# if __name__ == '__main__':
+#     generate_files()
