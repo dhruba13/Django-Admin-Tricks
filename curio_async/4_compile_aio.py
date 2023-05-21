3d0f34eee636ef50f7c07d5cd992aa535f9a5c58
Maxim <info@twinboards.at>
2023-05-21 08:54:58 +0200

    add other final resuts

--- curio_async/4_compile_aio.py
+++ curio_async/4_compile_aio.py
@@ -1,7 +1,34 @@
 from aiohttp.web import Application, run_app, StreamResponse, Response
 
-# HEADERS = b'HTTP/1.1 200 OK\r\nContent-type: text/html; charset=utf-8\r\nConnection: close\r\n\r\n'
-BODY = '<!DOCTYPE html><html><head><meta charset="utf-8"><title></title></head><body>Hi</body></html>'
+BODY_START = '<!DOCTYPE html><html><head><meta charset="utf-8"><title></title></head><body>'
+BODY_END = '</body></html>'
+
+import asyncio
+import time
+import aiofiles
+import hashlib
+
+from europython2023.curio_async import FILE_LIST, REPORT
+from europython2023.curio_async import REPORT as print
+
+
+async def render(stream, data, idx):
+    print('render start', idx)
+    text = data.decode('utf-8')
+    sha1 = hashlib.sha1(data)  # .update(data)
+    text = f'<pre><code>{text}</code></pre><p>Sha1: <samp>{sha1.hexdigest()}</samp><p>'.encode('utf-8')
+    print('rendered', idx)
+    # await stream.write(text)
+    return text, idx
+
+
+async def read(filename, idx):
+    print('started', filename, idx)
+    async with aiofiles.open(f'{filename}', mode='rb') as file:
+        try:
+            return (await file.read(), idx)
+        finally:
+            print('readed', filename, idx)
 
 
 async def echo_client(request):
@@ -14,8 +41,27 @@ async def echo_client(request):
     stream.content_type = 'text/html'
     stream.charset= 'utf-8'
     await stream.prepare(request)
-    #await stream.write(HEADERS)
-    await stream.write(BODY.encode('utf-8'))
+    await stream.write(BODY_START.encode('utf-8'))
+
+    tasks = []
+    for idx, filename in enumerate(FILE_LIST):
+        tasks.append(asyncio.create_task(read(filename, idx)))
+
+    response = await asyncio.gather(*tasks, return_exceptions=True)
+
+    tasks = []
+    for result in response:
+        tasks.append(asyncio.create_task(render(stream, *result)))
+
+    response = await asyncio.gather(*tasks, return_exceptions=True)
+
+    tasks = []
+    for text, idx in response:
+        tasks.append(asyncio.create_task(stream.write(text)))
+
+    response = await asyncio.gather(*tasks, return_exceptions=True)
+
+    await stream.write(BODY_END.encode('utf-8'))
     await stream.write_eof()
 
 if __name__ == '__main__':
